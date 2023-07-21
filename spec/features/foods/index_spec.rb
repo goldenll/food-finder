c7773be53b69ab86e1e6bb3a805bda8269457e48
Lauren Golden <llgolden@gmail.com>
2023-07-21 11:02:28 -0600

    Final commit

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -7,15 +7,10 @@ RSpec.describe "foods index page", type: :feature do
     click_button "Find Foods"
 
     expect(current_path).to eq(foods_path)
-save_and_open_page
+
     expect(page).to have_content("8901020020844")
     expect(page).to have_content("SWEET POTATOES")
     expect(page).to have_content("NOT A BRANDED ITEM")
     expect(page).to have_content("ORGANIC SWEET POTATOES.")
   end
 end
-
-# :gtinUpc=>"8901020020844"
-# :description=>"SWEET POTATOES"
-# :brandOwner=>"NOT A BRANDED ITEM"
-# :ingredients=>"ORGANIC SWEET POTATOES."
